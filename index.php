184da690a5f7c56ab557bdeaebc3cda919a431d0
dmlcks1009 <dmlcks14@gmail.com>
2021-11-26 16:23:51 +0900

    commit

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ include $_SERVER['DOCUMENT_ROOT'] . "/solid/db/create_statement.php";
 
 <head>
     <title>No.1 가상자산 플랫폼, Solid</title>
-    <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/solid/FTXcss/FTXmain.css?.afkqwekkster">
-    <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/solid/FTXcss/FTXfooter.css?.aqwesd">
-    <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/solid/FTXcss/FTXheader.css?.5swqesda">
-    <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/solid/FTXcss/FTXmain_section.css?swqesa">
+    <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/solid/FTXcss/FTXmain.css?.afkqwesadkkster">
+    <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/solid/FTXcss/FTXfooter.css?.aqwessadd">
+    <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/solid/FTXcss/FTXheader.css?.5swqeasdsda">
+    <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/solid/FTXcss/FTXmain_section.css?swqesadsa">
 </head>
 
 <body>
@@ -24,7 +24,7 @@ include $_SERVER['DOCUMENT_ROOT'] . "/solid/db/create_statement.php";
         <header>
             <?php include "./header.php"; ?>
         </header>
-        <section id="main-section">
+        <section class="mainsection" id="main-section">
             <?php include "./home.php"; ?>
         </section>
         <footer>
