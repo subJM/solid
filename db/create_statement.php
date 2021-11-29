ebbd76ca315e91eb70bb610e5bb6ed4f3e9afb34
thswhdals <thswhdals@naver.com>
2021-11-29 22:36:29 +0900

    jongmin

--- db/create_statement.php
+++ db/create_statement.php
@@ -1,20 +1,18 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . "/solid/db/db_connector.php";
 include $_SERVER['DOCUMENT_ROOT'] . "/solid/db/create_table.php";
-// include $_SERVER['DOCUMENT_ROOT'] . "/solid/db/create_procedure.php";
+include $_SERVER['DOCUMENT_ROOT'] . "/solid/db/create_procedure.php";
 include $_SERVER['DOCUMENT_ROOT'] . "/solid/db/create_trigger.php";
-// include $_SERVER['DOCUMENT_ROOT'] . "/solid/db/drop_procedure_scheduler.php";
 
 create_table($con, "members");
 create_table($con, "deleted_members");
 create_table($con, "coin_info");
 create_table($con, "favorite_coin");
 create_table($con, "notice");
-create_table($con, "review");
-create_table($con, "faq");
-create_table($con, "faq_ripple");
 create_table($con, "free");
 create_table($con, "free_ripple");
+create_table($con, "faq");
+create_table($con, "faq_ripple");
 create_table($con, "question");
 create_table($con, "question_ripple");
 
