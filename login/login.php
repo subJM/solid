a6bd0f4f954e5ab673fa6ae89aa8b95096c6faa6
thswhdals <thswhdals@naver.com>
2021-11-30 14:16:59 +0900

    jongmin

--- login/login.php
+++ login/login.php
@@ -2,52 +2,6 @@
 include $_SERVER['DOCUMENT_ROOT'] . "/solid/db/db_connector.php";
 
 if(!isset($_POST['client'])){
-$id   = $_POST["id"];
-$password = $_POST["password"];
-
-$sql = "select * from members where id='$id'";
-$result = mysqli_query($con, $sql);
-
-$num_match = mysqli_num_rows($result);
-
-if (!$num_match) {
-  echo ("
-         <script>
-           window.alert('등록되지 않은 아이디입니다!')
-           history.go(-1)
-         </script>
-       ");
-} else {
-  $row = mysqli_fetch_array($result);
-  $db_pass = $row["password"];
-
-  if (!$db_pass) {
-    echo ("
-            <script>
-              console.log('$db_pass');
-              window.alert('비밀번호가 틀립니다!')
-              history.go(-1)
-            </script>
-         ");
-
-    mysqli_close($con);
-    exit;
-  } else {
-    session_start();
-    $_SESSION["user_id"] = $row["id"];
-    $_SESSION["user_name"] = $row["name"];
-    $_SESSION["user_level"] = $row["level"];
-
-    echo ("
-            <script>
-              location.href = '../index.php';
-            </script>
-          ");
-
-    mysqli_close($con);
-  }
-}
-}else{
   $id   = $_POST["id"];
   $password = $_POST["password"];
 
@@ -56,27 +10,82 @@ if (!$num_match) {
 
   $num_match = mysqli_num_rows($result);
 
-  if (!$num_match) {
-    echo false;
-  } else {
-    $row = mysqli_fetch_array($result);
-    $db_pass = $row["password"];
-
-    if (!$db_pass) {
-      echo false;
-
-      mysqli_close($con);
-      exit;
+    if (!$num_match) {
+      echo ("
+            <script>
+              window.alert('등록되지 않은 아이디입니다!')
+              history.go(-1)
+            </script>
+          ");
     } else {
-      session_start();
-      $_SESSION["user_id"] = $row["id"];
-      $_SESSION["user_name"] = $row["name"];
-      $_SESSION["user_level"] = $row["level"];
+      $row = mysqli_fetch_array($result);
+      $db_pass = $row["password"];
 
-      echo true;
+      if (!$db_pass) {
+        echo ("
+                <script>
+                  console.log('$db_pass');
+                  window.alert('비밀번호가 틀립니다!')
+                  history.go(-1)
+                </script>
+            ");
 
-      mysqli_close($con);
+        mysqli_close($con);
+        exit;
+      } else {
+        session_start();
+        $_SESSION["user_id"] = $row["id"];
+        $_SESSION["user_name"] = $row["name"];
+        $_SESSION["user_level"] = $row["level"];
+
+        echo ("
+                <script>
+                  location.href = '../index.php';
+                </script>
+              ");
+
+        mysqli_close($con);
+      }
+    }
+  }else{
+    $id   = $_POST["id"];
+    $password = $_POST["password"];
+
+    $sql = "select * from members where id='$id'";
+    $result = mysqli_query($con, $sql);
+
+    $num_match = mysqli_num_rows($result);
+
+    if (!$num_match) {
+        echo json_encode(array(
+          "status" => "false",
+          "message" => "ID missing!"
+      ));
+    } else {
+      $row = mysqli_fetch_array($result);
+      $db_pass = $row["password"];
+
+      if (!$db_pass) {
+        echo json_encode(array(
+          "status" => "false",
+          "message" => "Password missing!"
+        ));
+
+        mysqli_close($con);
+        exit;
+      } else {
+        session_start();
+        $_SESSION["user_id"] = $row["id"];
+        $_SESSION["user_name"] = $row["name"];
+        $_SESSION["user_level"] = $row["level"];
+
+        echo json_encode(array(
+          "status" => "true",
+          "message" => "Success!"
+        ));
+
+        mysqli_close($con);
+      }
     }
   }
-}
 ?>
