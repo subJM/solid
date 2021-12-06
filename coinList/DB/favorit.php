663fd70b3c0cab2ccbd40f6fc7d5c5b1d08e68f8
printf2475 <printf2475@naver.com>
2021-12-06 14:19:08 +0900

    즐겨찾기 모바일업데이트

--- coinList/DB/favorit.php
+++ coinList/DB/favorit.php
@@ -3,8 +3,8 @@ include $_SERVER['DOCUMENT_ROOT'] . "/solid/db/db_connector.php";
 
 // $user_id = $_SESSION['user_id'];
 $user_id = "master";
-
 $sql = "SELECT * FROM favorite_coin";
+// $sql = "SELECT * FROM favorite_coin WHERE id=$user_id";
 // WHERE id ='$user_id'
 $list = array();
 $result = mysqli_query($con, $sql) or die("검색 ERROR" . mysqli_error($con));
@@ -13,6 +13,10 @@ while ($row = mysqli_fetch_array($result)) {
     $list[$count++] = $row['coinName'];
 }
 
-echo "let favorit_coinName = " . json_encode($list);
+if (!isset($_POST['client'])) {
+    echo "let favorit_coinName = " . json_encode($list);
+}else {
+echo json_encode($list);
+}
 
 mysqli_close($con);
