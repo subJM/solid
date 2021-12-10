3d11efdc4ba9c6a102b439c2b225a2902e26c826
OhSehyung <osh612@gmail.com>
2021-12-10 14:10:07 +0900

    a

--- header.php
+++ header.php
@@ -1,14 +1,14 @@
 <!-- <?php
-if (session_status() == PHP_SESSION_NONE) {
-	session_start();
-}
-if (isset($_SESSION["user_id"])) $userid = $_SESSION["user_id"];
-else $userid = "";
-if (isset($_SESSION["user_name"])) $username = $_SESSION["user_name"];
-else $username = "";
-if (isset($_SESSION["user_level"])) $userlevel = $_SESSION["user_level"];
-else $userlevel = "";
-?> -->
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+        if (isset($_SESSION["user_id"])) $userid = $_SESSION["user_id"];
+        else $userid = "";
+        if (isset($_SESSION["user_name"])) $username = $_SESSION["user_name"];
+        else $username = "";
+        if (isset($_SESSION["user_level"])) $userlevel = $_SESSION["user_level"];
+        else $userlevel = "";
+        ?> -->
 
 
 <div class="top1">
@@ -20,42 +20,42 @@ else $userlevel = "";
             <li class="top1-1tabletd" href="http://<?= $_SERVER['HTTP_HOST'] ?>/solid/member/member_form.php">
                 공지사항
             </li>
-            <li class="top1-1tabletd">자산</li>
+            <li class="top1-1tabletd" href="http://<?= $_SERVER['HTTP_HOST'] ?>/solid/wallet/wallet.html">자산</li>
             <li class="top1-1tabletd">1:1문의</li>
         </ul>
     </div>
     <div class="top1-2">
         <ul class="top1-2table">
             <?php
-			if (!$userid) {
-                ?>
-            <li class="top1-2tabletd"><a
+            if (!$userid) {
+            ?>
+            <li class=" top1-2tabletd"><a
                     href="http://<?= $_SERVER['HTTP_HOST'] ?>/solid/member/member_form.php">회원가입</a></li>
             <li class="top1-2tabletd" href="http://<?= $_SERVER['HTTP_HOST'] ?>/solid/login/login_form.php">
                 로그인</a>
             </li>
             <?php
-			} else {
-				$logged = $username . "(" . $userid . ")님 환영합니다.";
-			?>
+            } else {
+                $logged = $username . "(" . $userid . ")님 환영합니다.";
+            ?>
             <li><?= $logged ?></li>
             <li>
                 < class="top1-2tabletd" href="http://<?= $_SERVER['HTTP_HOST'] ?>/solid/login/logout.php">로그아웃
             </li>
             <?php
-			}
+            }
             if ($userid && $userlevel == 1) { ?>
             <li>
                 < href="http://<?= $_SERVER['HTTP_HOST'] ?>/solid/admin/admin_members.php">관리자모드
             </li>
             <?php
-			} else if ($userid) {
-			?>
+            } else if ($userid) {
+            ?>
             <li><a href="http://<?= $_SERVER['HTTP_HOST'] ?>/solid/member/member_mypage.php">마이페이지
             </li>
             <?php
-			}
-			?>
+            }
+            ?>
         </ul>
     </div>
 </div>
--- purchasehistory.php
+++ purchasehistory.php
@@ -6,7 +6,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../solid/WebContents/css/bootstrap.css">
-    <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/solid/FTXcss/FTXmain.css">
     <script type=" text/javascript" src="/WebContents/js/bootstrap.js">
     </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -233,8 +232,12 @@
 </head>
 
 <body>
-    <header>
-        <?php include "./header.php"; ?>
+    <header class="header">
+        <div class="topnav">
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+        </div>
     </header>
     <div class="row">
         <div class="row_container">
@@ -249,77 +252,57 @@
             <div class=" column_middle">
                 <div class="middle_container">
                     <div class="middle_container_name">
-                        <h2 class="main_name">수익현황</h2>
-                    </div>
-                    <div class="middle_overall">
-                        <div class="overall_1">
-                            <div class="overall_1_1">총 보유자산</div>
-                            <div class="overall_1_2">0 원</div>
-                        </div>
-                        <div class="overall_2">
-                            <div class="overall_2_1">
-                                <div class="overall_2_1_1">
-                                    <div class="tag_1">보유 원화</div>
-                                    <div class="value_1">0원</div>
-                                </div>
-                                <div>
-                                    <div class="tag_1">보유 가상자산</div>
-                                    <div class="value_1">0원</div>
-                                </div>
-                            </div>
-                            <div class="overall_2_1">
-                                <div>
-                                    <div class="tag_2">총 매수금액</div>
-                                    <div class="value_2">0원</div>
-                                </div>
-                                <div>
-                                    <div class="tag_2">평가 손익</div>
-                                    <div class="value_2">0원</div>
-                                </div>
-                                <div>
-                                    <div class="tag_2">수익률</div>
-                                    <div class="value_3">0.00%</div>
-                                </div>
-                            </div>
-                        </div>
+                        <h2 class="main_name">이용내역</h2>
                     </div>
                     <div class="table_comment">매수평균가, 평가금액, 평가손익, 수익률은 모두 KRW로 환산한 추정 값으로 참고용입니다</div>
                     <div>
                         <table class="table">
                             <thead class="table-light">
                                 <tr>
-                                    <th scope="col">코인명</th>
-                                    <th class="col_r">보유수량</th>
-                                    <th class="col_r">매수 평균가</th>
-                                    <th class="col_r">매수 금액</th>
-                                    <th class="col_r">평가 금액</th>
-                                    <th class="col_r">수익률</th>
+                                    <th scope="col">시간</th>
+                                    <th class="col_r">마켓</th>
+                                    <th class="col_r">코인</th>
+                                    <th class="col_r">거래</th>
+                                    <th class="col_r">주문 및 체결</th>
+                                    <th class="col_r">체결량</th>
+                                    <th class="col_r">체결가</th>
+                                    <th class="col_r">거래금액</th>
+                                    <th class="col_r">수수료</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td class="td_l">XRP</td>
-                                    <td>0.000009</td>
-                                    <td>263.2원</td>
-                                    <td>0원</td>
-                                    <td>0원</td>
-                                    <td>+404.1%</td>
+                                    <td class="td_l"></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
                                 </tr>
                                 <tr>
-                                    <td class="td_l">XRP</td>
-                                    <td>0.000009</td>
-                                    <td>263.2원</td>
-                                    <td>0원</td>
-                                    <td>0원</td>
-                                    <td>+404.1%</td>
+                                    <td class="td_l"></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
                                 </tr>
                                 <tr>
-                                    <td class="td_l">XRP</td>
-                                    <td>0.000009</td>
-                                    <td>263.2원</td>
-                                    <td>0원</td>
-                                    <td>0원</td>
-                                    <td>+404.1%</td>
+                                    <td class="td_l"></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
                                 </tr>
                             </tbody>
                         </table>
